34df112fa7e983f2c31396bd4ea7623586f69550
Кирилл <shmitkokirill@gmail.com>
2022-02-17 18:58:11 +0300

    Practice1/Task4_2?incorrectImagePaste

--- Practice1/app/src/main/java/com/example/practice1/MainActivity.java
+++ Practice1/app/src/main/java/com/example/practice1/MainActivity.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-    }
+   }
 }
